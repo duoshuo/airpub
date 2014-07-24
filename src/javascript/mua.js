3e0a02b7c8fc6724559adef5968ff77bc96b105e
Guo Yu <i@guoyu.me>
2014-07-24 16:26:56 +0800

    0.1: 添加默认主题

--- src/javascript/mua.js
+++ src/javascript/mua.js
@@ -1,31 +1,35 @@
-angular.module('mua', ['ui.bootstrap', 'ui.router'])
-  config(function($stateProvider, $urlRouterProvider) {
+angular.module('mua', ['ui.bootstrap', 'ui.router', 'snap'])
+  .config(function($stateProvider, $urlRouterProvider) {
     $urlRouterProvider.otherwise("/404");
     $stateProvider
       .state('home', {
         url: "",
         templateUrl: "views/archive.html"
       })
-      .state('archive', {
-        url: "/archive",
-        templateUrl: "views/archive.html"
-      })
       .state('single', {
         url: "/article",
-        templateUrl: "views/article.html"
+        templateUrl: "views/single.html"
+      })
+      .state('404', {
+        url: "/404",
+        templateUrl: "views/404.html"
       })
   })
-  .controller('alertCtrler', function($scope) {
-    $scope.alerts = [{
-      type: 'danger',
-      msg: 'Oh snap! Change a few things up and try submitting again.'
-    }];
-    $scope.addAlert = function() {
-      $scope.alerts.push({
-        msg: 'Another alert!'
-      });
-    };
-    $scope.closeAlert = function(index) {
-      $scope.alerts.splice(index, 1);
-    };
+  .controller('articlesCtrler', function($scope) {
+    $scope.articles = [{
+      title: '已经有 2000 行了',
+      uri: '/200x',
+      content: 'django - models.py 已经有 2000 行了，有什么办法，能分割成一个 module，一个文件夹多个文件的形式呢？django - models.py 已经有 2000 行了，有什么办法，能分割成一个 module，一个文件夹多个文件的形式呢？django - models.py 已经有 2000 行了，有什么办法，能分割成一个 module，一个文件夹多个文件的形式呢？django - models.py 已经有 2000 行了，有什么办法，能分割成一个 module，一个文件夹多个文件的形式呢？'
+    }, {
+      title: 'django',
+      uri: '/django',
+      content: 'django - models.py 已经有 2000 行了，有什么办法，能分割成一个 module，一个文件夹多个文件的形式呢？django - models.py 已经有 2000 行了，有什么办法，能分割成一个 module，一个文件夹多个文件的形式呢？django - models.py 已经有 2000 行了，有什么办法，能分割成一个 module，一个文件夹多个文件的形式呢？django - models.py 已经有 2000 行了，有什么办法，能分割成一个 module，一个文件夹多个文件的形式呢？'
+    }]
+  })
+  .controller('singleArticlesCtrler', function($scope) {
+    $scope.article = {
+      title: 'django',
+      uri: '/django',
+      content: 'django - models.py 已经有 2000 行了，有什么办法，能分割成一个 module，一个文件夹多个文件的形式呢？django - models.py 已经有 2000 行了，有什么办法，能分割成一个 module，一个文件夹多个文件的形式呢？django - models.py 已经有 2000 行了，有什么办法，能分割成一个 module，一个文件夹多个文件的形式呢？django - models.py 已经有 2000 行了，有什么办法，能分割成一个 module，一个文件夹多个文件的形式呢？'
+    }
   })

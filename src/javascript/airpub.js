97d9fb7b786dc25b9b22403dec8cbb05ce74df3e
Guo Yu <i@guoyu.me>
2014-07-28 20:55:38 +0800

    0.1: update admin views

--- src/javascript/airpub.js
+++ src/javascript/airpub.js
@@ -37,24 +37,28 @@
   });
   // archive ctrler
   app.controller('articles', function($scope, $state) {
+    // read from cache
     if ($scope.articles && $scope.articles.length > 0) return;
+    // read from fresh
     NProgress.start();
     database.get('threads/list', {
-      limit: 30,
-      page: 1
+      page: 1,
+      limit: 30
     }, function(data) {
       NProgress.done();
       $scope.$parent.ready = true;
       if (data.code !== 0) $scope.addAlert('danger', '获取信息失败，请重试');
       $scope.articles = data.response || [];
       $scope.$apply();
+      if ($scope.articles.length === 0) return $state.go('404');
       return;
     });
   });
   // article ctrler
   app.controller('article', function($scope, $state) {
     var uri = $state.params.uri;
-    if (!uri) return; // TODO: go 404
+    if (!uri) return $state.go('404');
+    // todo: fetch article via thread_id
     return;
   });
   // pageer ctrler
@@ -72,13 +76,17 @@
     $scope.bigCurrentPage = 1;
   });
   // all ui behaviors
-  app.controller('ui', function($scope) {
+  app.controller('ui', function($scope, $state, $timeout) {
+    $scope.state = $state;
+    // alerts module
     $scope.alerts = [];
-    $scope.addAlert = function(type, msg) {
-      $scope.alerts.push({
-        msg: msg,
-        type: type || 'success'
-      });
+    $scope.addAlert = function(type, msg, dismiss) {
+      $scope.alerts.push({ msg: msg, type: type || 'success' });
+      var alertIndex = $scope.alerts.length - 1;
+      if (!dismiss) return alertIndex;
+      $timeout(function() {
+        $scope.closeAlert(alertIndex);
+      }, 5000);
     };
     $scope.closeAlert = function(index) {
       $scope.alerts.splice(index, 1);
@@ -86,6 +94,21 @@
   });
   // admin ctrler
   app.controller('admin', function($scope, $state) {
-
+    if ($scope.user) return;
+    console.log($state.current);
+    // signin status check
+    duoshuo.visitor.on('reset', function(){
+      var visitor = this.data.user_id === 0;
+      // redirect visitors
+      if (visitor) { 
+        $scope.addAlert('danger','抱歉，你还没有登录哦');
+        $scope.$apply();
+        $state.go('home');
+        return;
+      }
+      // fullfill user data
+      $scope.user = this.data;
+      $scope.$apply();
+    });
   });
 })(window.DUOSHUO);

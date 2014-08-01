410b67b1ab72ff54b683878762fe3cc71d741691
Guo Yu <i@guoyu.me>
2014-08-01 20:02:11 +0800

    0.1: update editor, remove cm dep

--- src/javascript/controllers/admin.js
+++ src/javascript/controllers/admin.js
@@ -1,7 +1,7 @@
 // admin ctrler
 airpub.controller('admin', function($scope, $state, $upyun, $duoshuo) {
-  // clear progress
-  NProgress.done();
+  var editor = new Editor();
+  editor.render();
   $scope.createArticle = function() {
     console.log($scope.article);
     return;

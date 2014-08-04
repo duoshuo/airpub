85e76c62e1aa36c2f93efb061641970b0f28ecdb
Guo Yu <i@guoyu.me>
2014-08-04 22:07:46 +0800

    0.1: add cloak

--- src/javascript/controllers/admin.js
+++ src/javascript/controllers/admin.js
@@ -1,7 +1,11 @@
 // admin ctrler
 airpub.controller('admin', function($scope, $state, $upyun, $duoshuo) {
-  $duoshuo.get('sites/membership', {}, function(){
-    
+  // todo: error handler
+  $duoshuo.get('sites/membership', {}, function(data){
+    var isOk = data.code === 0;
+    if (!isOk || data.response.role !== 'administrator')
+      return $state.go('404');
+    $scope.isAdmin = true;
   });
   $scope.createArticle = function() {
     console.log($scope.article);

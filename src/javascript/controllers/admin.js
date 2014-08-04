0059bfec2ce46c193f5d79a153e59fde1742d13d
Guo Yu <i@guoyu.me>
2014-08-04 21:51:55 +0800

    0.1: bugfix, split duoshuoQuery from airpubConfigs

--- src/javascript/controllers/admin.js
+++ src/javascript/controllers/admin.js
@@ -1,6 +1,8 @@
 // admin ctrler
 airpub.controller('admin', function($scope, $state, $upyun, $duoshuo) {
-  console.log($duoshuo);
+  $duoshuo.get('sites/membership', {}, function(){
+    
+  });
   $scope.createArticle = function() {
     console.log($scope.article);
   };
--- src/javascript/controllers/global.js
+++ src/javascript/controllers/global.js
@@ -2,8 +2,5 @@
 airpub.controller('global', function($scope) {
   if (!airpubConfigs) 
     return console.error(new Error('airpub 缺少必要的配置文件!'));
-  if (!airpubConfigs.duoshuo)
-    return console.error(new Error('airpub 缺少多说配置文件!'));
   $scope.configs = airpubConfigs;
-  window.duoshuoQuery = airpubConfigs.duoshuo;
 });

f8cdc19f6ccc3010c2165d4447e1257e7f7d124a
Guo Yu <i@guoyu.me>
2014-08-06 17:54:01 +0800

    0.1: remove controller pager

--- src/javascript/controllers/archive.js
+++ src/javascript/controllers/archive.js
@@ -1,5 +1,7 @@
 // archive ctrler
 airpub.controller('archive', function($scope, $state, $duoshuo) {
+  $scope.itemsPerPage = 5;
+  $scope.currentPage = 1;
   // read from cache
   if ($scope.articles && $scope.articles.length > 0) return;
   // read from fresh
@@ -15,15 +17,15 @@ airpub.controller('archive', function($scope, $state, $duoshuo) {
   };
 
   function fetchThreads(options) {
-    options.limit = 10;
+    options.limit = $scope.itemsPerPage;
     $duoshuo.get('threads/list', options, function(err, result, res) {
       if (err) 
         return $scope.addAlert('获取信息失败，请重试', 'danger');
       if (result.length === 0) 
         return $state.go('404');
       $scope.articles = result || [];
-      if (!$scope.totalItems) 
-        $scope.totalItems = res.cursor.total;
+      $scope.totalItems = res.cursor.total;
+      console.log(res)
       return;
     });
   }

5ca70f882131bf7c63c6a2c5bf546b93707c49e8
Guo Yu <i@guoyu.me>
2014-08-15 01:27:00 +0800

    0.1: bugfix: add a ugly hack for pager

--- src/javascript/controllers/archive.js
+++ src/javascript/controllers/archive.js
@@ -1,28 +1,36 @@
 // archive ctrler
 airpub.controller('archive', function($scope, $state, $duoshuo) {
-  $scope.itemsPerPage = 1;
+  $scope.itemsPerPage = 10;
   $scope.currentPage = parseNumber($state.params.page) || 1;
+
   // read from cache
   if ($scope.articles && $scope.articles.length > 0) return;
+  // TODO: remove this ugly hack
+  if ($state.params.page) {
+    var lock = true;
+    var currentPage = $scope.currentPage;
+  }
   // read fresh  
   $duoshuo.get('threads/list', {
     page: $scope.currentPage,
     limit: $scope.itemsPerPage,
     with_content: 1
   }, function(err, result, res) {
+    if ($state.params.page) 
+      lock = false;
     if (err)
       return $scope.addAlert('获取信息失败，请重试', 'danger');
     if (result.length === 0)
       return $state.go('404');
     $scope.articles = result || [];
     $scope.totalItems = res.cursor.total;
+    if ($state.params.page) $scope.currentPage = currentPage;
     return;
   });
   // when page changed, go => /#/page/currentPage
   // why the fucking event was trigged twice and return `1` the second time ?!
   $scope.pageChanged = function() {
-    console.log($scope.currentPage);
-    if ($state.params.page) return;
+    if (lock) return;
     $state.go('pager', {
       page: $scope.currentPage
     });

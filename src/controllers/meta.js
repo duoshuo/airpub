8926933435d4885b7b668f61048f5a0de98db5c5
Guo Yu <i@guoyu.me>
2014-10-24 20:22:35 +0800

    0.4: Init Wechat.js when airpub ready

--- src/controllers/meta.js
+++ src/controllers/meta.js
@@ -8,6 +8,7 @@
   function metaCtrler($scope, $rootScope) {
     $scope.title = $scope.configs.name || 'Airpub';
     $scope.description = $scope.configs.description || 'just another Airpub blog';
+    initWeixinShare($scope.configs);
 
     $rootScope.$on('updateMeta', function(eve, data){
       if (typeof(data) === 'string')
@@ -16,5 +17,21 @@
         $scope[k] = v;
       });
     });
+  }
+
+  function initWeixinShare(metaData) {
+    if (!window.wechat)
+      return;
+
+    var wechat = window.wechat;
+    var data = {};
+
+    if (metaData.url) data.link = metaData.url
+    if (metaData.name) data.title = metaData.name;
+    if (metaData.description) data.desc = metaData.description;
+
+    wechat('friend', data);
+    wechat('timeline', data);
+    wechat('weibo', data);
   }
 })(window.angular);

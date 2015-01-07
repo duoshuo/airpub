045a78d861f7b4c2b2c6f7c7ddd6f01262cd5429
Guo Yu <i@guoyu.me>
2015-01-07 21:10:48 +0800

    Complete the custom renderer for marked.js

--- src/filters/marked.js
+++ src/filters/marked.js
@@ -1,104 +1,129 @@
-;(function(angular, debug) {
+;
+(function(angular, debug) {
   'use strict';
-  
+
   angular
     .module('airpub')
     .filter('marked', ['$sce', '$sanitize', markedFilter]);
 
   function markedFilter($sce, $sanitize) {
     return function(raw) {
-      if (!raw) 
+      if (!raw)
         return '';
-      if (!marked) 
-        throw new Error('marked.js required!');
+      if (!marked)
+        throw new Error('Marked.js is required!');
+
+      var wrappers;
+
+      // Check if additional filter params is provided.
+      if (arguments.length > 1)
+        wrappers = parseWrapperString(Array.prototype.slice.call(arguments, 1));
 
       var markedOptions = {};
 
-      // Setting: highlight
-      if (window.hljs) 
-        markedOptions.highlight = highlightCode;
-
-      // Setting: custom renderer
+      // Make a custom Renderer for marked
       var render = new marked.Renderer();
 
+      // Extends from code prototye but let user to choose 
+      // Use unescape html or not.
       render.code = function(code, lang, escaped) {
-        var outputRaw = (lang === 'raw-html' || this.options.highlight);
+        var html = marked.Renderer.prototype.code.call(
+          this, 
+          (lang === 'unsafe-html') ? $sanitize(code) : code, 
+          lang, 
+          escaped
+        );
 
-        if (this.options.highlight) {
-          var out = this.options.highlight(code, lang);
-          if (out != null && out !== code) {
-            escaped = true;
-            code = out;
-          }
+        if (wrappers && wrappers.code)
+          return injectTo(wrappers.code, html)
+
+        return html;
+      };
+
+      // Extends from other prototypes
+      angular.forEach([
+        'blockquote',
+        'html',
+        'heading',
+        'list',
+        'listitem',
+        'paragraph',
+        'table',
+        'tablerow',
+        'tablecell',
+        'image'
+      ], function(type){
+        render[type] = function() {
+          var html = marked.Renderer.prototype[type].apply(this, arguments);
+
+          if (wrappers && wrappers[type])
+            return injectTo(wrappers[type], html)
+
+          if (wrappers && wrappers.all)
+            return injectTo(wrappers.all, html)
+
+          return html;
         }
-
-        return wrapwith(
-          'code-section',
-          '<pre>' +
-            '<code class="' + lang + '" ng-bind="sqwswqsw">' + 
-              (outputRaw ? code : $sanitize(code)) + 
-            '</code>' +
-          '</pre>'
-        );
-      };
-      render.html = function(html) {
-        return wrapwith(
-          'html-section',
-          html
-        );
-      };
-      render.heading = function(str, level) {
-        var realLevel = level;
-        return wrapwith(
-          'heading-section',
-          '<h' + realLevel + '>' + str + '</h' + realLevel + '>'
-        );
-      };
-      render.paragraph = function(str) {
-        return wrapwith(
-          'paragraph-section',
-          '<p>' + str + '</p>'
-        );
-      };
-      render.blockquote = function(str) {
-        return wrapwith(
-          'blockquote-section',
-          '<blockquote>' + str + '</blockquote>'
-        );
-      };
-      render.list = function(str, ordered) {
-        var wrapper = ordered ? 'o' : 'u';
-        return wrapwith(
-          'blockquote-section',
-          '<' + wrapper + 'l>' + str + '</' + wrapper + 'l>'
-        );
-      };
+      })
 
       markedOptions.renderer = render;
-      marked.setOptions(markedOptions);
 
-      // Helpers
-      // TODO: Remove hard code DOM here.
-      function wrapwith(wrapperClass, dom) {
-        wrapperClass =
-          typeof(wrapperClass) === 'object' ?
-          wrapperClass.join(' ') :
-          wrapperClass;
-        return [
-          '<section class="' + wrapperClass + '">',
-            '<div class="container">',
-              '<div class="row">',
-                '<div class="col-lg-8 col-lg-offset-2">',
-                  dom,
-                '</div>',
-              '</div>',
-            '</div>',
-          '</section>'
-        ].join('\n');
-      }
+      // Setting: highlight
+      if (window.hljs)
+        markedOptions.highlight = highlightCode;
+
+      marked.setOptions(markedOptions);
 
       function highlightCode(code) {
         return window.hljs.highlightAuto(code).value;
+      }
+
+      function parseWrapperString(wrappers) {
+        var parsed = {};
+
+        wrappers.forEach(function(str) {
+          var key = str.indexOf(':') === -1 ?
+            'all' : str.split(':')[0];
+
+          parsed[key] = str.split(':')[1];
+        });
+
+        return parsed;
+      }
+
+      function injectTo(str, entryHtml) {
+        function parseName(str) {
+          var arr = str.split(' ')
+          if (arr.length === 1)
+            return str.substr(1);
+
+          var ret = '';
+          angular.forEach(arr, function(token) {
+            ret += token.substr(1) + ' ';
+          });
+          return ret;
+        }
+
+        var domNotes = str.split('>');
+
+        if (!domNotes.length)
+          return entryHtml;
+
+        var wraps = '';
+
+        angular.forEach(domNotes, function(item) {
+          var attrName = item.charAt(0) === '.' ?
+            'class' : 'id';
+          wraps += '<div ' + attrName + '="' + parseName(item) + '">';
+        });
+
+        wraps += entryHtml;
+
+        angular.forEach(domNotes, function() {
+          wraps += '</div>';
+        });
+
+        return wraps;
       }
 
       return $sce.trustAsHtml(marked(raw));

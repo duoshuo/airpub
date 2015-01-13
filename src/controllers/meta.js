143b9f073dfe85c658d5a6985d988b1b0b6449b5
Guo Yu <i@guoyu.me>
2015-01-13 16:51:19 +0800

    Using service share.js instead of share fn

--- src/controllers/meta.js
+++ src/controllers/meta.js
@@ -3,12 +3,23 @@
   
   angular
     .module('airpub')
-    .controller('meta', ['$scope', '$rootScope', metaCtrler]);
+    .controller('meta', [
+      '$scope', 
+      '$rootScope',
+      'share',
+      metaCtrler
+    ]);
 
-  function metaCtrler($scope, $rootScope) {
+  function metaCtrler($scope, $rootScope, share) {
     $scope.title = $scope.configs.name || 'Airpub';
     $scope.description = $scope.configs.description || 'just another Airpub blog';
-    initWeixinShare($scope.configs);
+
+    // Init service share
+    share.wechat({
+      title: $scope.configs.name,
+      link: $scope.configs.url,
+      desc: $scope.configs.description
+    });
 
     $rootScope.$on('updateMeta', function(eve, data){
       if (typeof(data) === 'string')
@@ -17,21 +28,5 @@
         $scope[k] = v;
       });
     });
-  }
-
-  function initWeixinShare(metaData) {
-    if (!window.wechat)
-      return;
-
-    var wechat = window.wechat;
-    var data = {};
-
-    if (metaData.url) data.link = metaData.url
-    if (metaData.name) data.title = metaData.name;
-    if (metaData.description) data.desc = metaData.description;
-
-    wechat('friend', data);
-    wechat('timeline', data);
-    wechat('weibo', data);
   }
 })(window.angular, window.debug);

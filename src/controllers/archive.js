a652a0daf815cd2f6dcd1f3d1d6f1de2c95eeda0
Guo Yu <i@guoyu.me>
2015-01-13 16:57:48 +0800

    Rewriting src/archive.js

--- src/controllers/archive.js
+++ src/controllers/archive.js
@@ -4,13 +4,18 @@
   angular
     .module('airpub')
     .controller('archive', [
-      '$scope', '$state', 'duoshuo', '$rootScope', 
+      '$scope', 
+      '$state', 
+      'duoshuo', 
+      '$rootScope', 
       archiveCtrler
     ]);
 
   function archiveCtrler($scope, $state, duoshuo, $rootScope) {
     $scope.itemsPerPage = 10;
     $scope.currentPage = parseNumber($state.params.page) || 1;
+
+    // Update document title
     $rootScope.$emit('updateMeta', $scope.configs.name);
 
     // Read articles from cache
@@ -24,53 +29,57 @@
     }
 
     // Read data from fresh  
-    duoshuo.get('threads/list', {
-      page: $scope.currentPage,
-      limit: $scope.itemsPerPage,
-      with_content: 1
-    }, function(err, result, res) {
-      if ($state.params.page)
-        lock = false;
-      if (err)
-        return $scope.addAlert('获取信息失败，请重试', 'danger');
-      if (result.length === 0)
-        return $state.go('layout.404');
-
-      $scope.articles = result || [];
-      $scope.totalItems = res.cursor.total;
-
-      if ($state.params.page) 
-        $scope.currentPage = currentPage;
-
-      return;
-    }, function(err) {
-      return $state.go('layout.404');
-    });
+    fetchFreshArticles();
 
     // When page changed, go => /#/page/currentPage
     // Why the fucking event was trigged twice and return `1` the second time ?!
-    $scope.pageChanged = function() {
-      if (lock) return;
+    $scope.pageChanged = pageChanged;
+
+    function fetchFreshArticles() {
+      var query = {};
+      query.page = $scope.currentPage;
+      query.limit = scope.itemsPerPage;
+      query.with_content = 1;
+
+      // Open a request
+      duoshuo.get('threads/list', query, onSuccess, onError);
+
+      function onSuccess(err, result, res) {
+        if ($state.params.page)
+          lock = false;
+        if (err)
+          return $scope.addAlert('获取信息失败，请重试', 'danger');
+        if (result.length === 0)
+          return $state.go('layout.404');
+
+        // Exporse locals to templates
+        $scope.articles = result || [];
+        $scope.totalItems = res.cursor.total;
+
+        if ($state.params.page) 
+          $scope.currentPage = currentPage;
+
+        return;
+      }
+
+      function onError(err) {
+        return $state.go('layout.404');
+      }
+    }
+
+    function pageChanged() {
+      if (lock) 
+        return;
+
       $state.go('layout.pager', {
         page: $scope.currentPage
       });
-    };
-
-    function initWeixinShare() {
-      if (!window.wechat)
-        return;
-
-      // Update share's link when page changes.
-      data.link = window.location.href;
-
-      window.wechat('friend', data);
-      window.wechat('timeline', data);
-      window.wechat('weibo', data);
     }
 
     function parseNumber(str) {
       if (str && !isNaN(parseInt(str)))
         return parseInt(str);
+
       return false;
     }
   }

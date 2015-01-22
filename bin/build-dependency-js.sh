3ffcb258e04bcb0437f83f52055d70193c07fd66
Guo Yu <i@guoyu.me>
2015-01-22 18:07:36 +0800

    Remove lib wechat.js since wechat do not support old js bridge anymore

--- bin/build-dependency-js.sh
+++ bin/build-dependency-js.sh
@@ -10,7 +10,6 @@ node_modules/.bin/uglifyjs \
   bower_components/nprogress/nprogress.js \
   bower_components/duoshuo/dist/duoshuo.min.js \
   bower_components/upyun/dist/upyun.min.js \
-  bower_components/wechat.js/wechat.js \
   --mangle \
   --compress \
   -o dist/airpub-dependencies.min.js \
